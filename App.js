0f1c38b090a6f9771110351839eed48ac6052734
Asaad Budayr <asad_289@hotmail.com>
2019-03-16 13:30:18 -0700

    Feat: Create the store
    Apply reducers and middleware

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import CameraComponent  from './components/Camera';
-
+import PictureList from "./components/PictureList"
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+import middleware from "./middleware";
 
 export default class App extends React.Component {
   render() {
     return (
+      <Provider store={createStore(reducer, middleware)}>
       <View style={styles.container}>
-        <CameraComponent />
+
+          <CameraComponent />
       </View>
+      </Provider>
     );
   }
 }
